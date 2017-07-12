86ecacaf4a02c0e50d7b14295336332d1071b66a
Mark Abella <mark.abella@gmail.com>
2017-07-12 13:46:02 +0000

    Add mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,13 +12,19 @@ class Kele
             email: email, 
             password: password 
         })
-    raise 'Unable to start sessoion' if response.code != 200
+    raise 'Unable to start session' if response.code != 200
     @auth_token = response["auth_token"]
   end
   
   def get_me
     response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
-    raise 'Unable to get info' if response.code != 200
+    raise 'Unable to get user info' if response.code != 200
     @user_data = JSON.parse(response.body)
+  end
+  
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    raise 'Unable to get mentor availability' if response.code != 200
+    @mentor_availability = JSON.parse(response.body)
   end
 end
